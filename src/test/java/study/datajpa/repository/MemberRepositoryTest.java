4dd1cbfbffc09df3917282db836c24c3514d5e9f
naegonggae <tkdtkd97@naver.com>
2023-06-27 17:58:42 +0900

    :memo: 공통 인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -37,5 +38,33 @@ class MemberRepositoryTest {
 		assertThat(findMember.getId()).isEqualTo(member.getId());
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 		assertThat(findMember).isEqualTo(savedMember);
+	}
+
+	@Test
+	void basicCRUD() {
+		Member member1 = new Member("memberA");
+		Member member2 = new Member("memberB");
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		// 단건조회 검증
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+
+		// 리스트 검증
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		// 카운트 검증
+		long count = memberRepository.count();
+		assertThat(count).isEqualTo(2);
+
+		// 삭제 검증
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
+		long deletedCount = memberRepository.count();
+		assertThat(deletedCount).isEqualTo(0);
 	}
 }
