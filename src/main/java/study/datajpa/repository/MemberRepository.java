4d947b2cee06e207d76a67e449cfd0665e9eb2c0
naegonggae <tkdtkd97@naver.com>
2023-06-27 18:10:18 +0900

    :memo: 공통 인터페이스 분석

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	List<Member> findByUsername(String username); // 쿼리 메서드 기능 그냥 알아서 JPA 가 만들어줌
 
 }
